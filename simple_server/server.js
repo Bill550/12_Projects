9f03ebe0fcb8a6751e937ff250d98ec019d9d244
Muhammad Bilal <bilalsuhail@rocketmail.com>
2020-06-01 20:48:50 +0500

    1st

---
+++ simple_server/server.js
@@ -0,0 +1,47 @@
+const http = require('http');
+const url = require('url');
+const path = require('path');
+const fs = require('fs');
+
+const mimetypes = {
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpg",
+    "png": "image/png",
+    "js": "text/javascript",
+    "css": "text/css"
+};
+
+http.createServer(function (req , res) {
+    var uri = url.parse(req.url).pathname;
+    var filename = path.join(process.cwd(), unescape(uri));
+    console.log("Loading" + uri);
+    var stats;
+
+    try {
+        stats = fs.lstatSync(filename);
+    } catch (e) {
+        res.writeHead(404,{'Content-type': 'text/plain'});
+        res.write('404 Not Found\n');
+        res.end();
+        return;
+    }
+
+    if (stats.isFile) {
+        var mimetype = mimetypes[path.extname(filename).split(".").reverse()[0]];
+        res.writeHead(200, { 'Content-type': mimetype });
+
+        var fileStream =fs.createReadStream(filename);
+        fileStream.pipe(res); 
+
+    } else if (stats.isDirectory()) {
+        res.writeHead(302, {'Location': 'index.html'});
+        res.end();
+
+    } else {
+        res.writeHead(500,{'Content-type': 'text/plain'});
+        res.write('500 internal Errors\n');
+        res.end();
+
+    }
+}).listen(3000);
